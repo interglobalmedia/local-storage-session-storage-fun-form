a291d0059c67e475135b24808ddfb2448f9248e4
interglobalmedia <interglobalmedia@gmail.com>
2020-04-08 10:04:03 -0400

    Add note related script and textarea related script

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -11,6 +11,20 @@ let fontStyleElem = document.querySelector(".font-style");
 // image related variables
 let imageSrcSelect = document.querySelector("#image");
 let imageElem = document.querySelector("img");
+// textarea related variables
+let textAreaElem = document.querySelector("textarea");
+let textAreaInput = document.querySelector("#textArea");
+let noteBtn = document.querySelector("#note-btn");
+let getNoteBtn = document.querySelector("#get-note-btn");
+
+noteBtn.addEventListener("click", () => {
+  localStorage.setItem("note", document.querySelector("#textArea").value);
+});
+
+getNoteBtn.addEventListener("click", () => {
+  localStorage.getItem("note", document.querySelector("#textArea").value);
+  document.querySelector("#textArea").value = localStorage.getItem("note");
+});
 
 if (!localStorage.getItem("bgcolor")) {
   populateStorage();
@@ -23,6 +37,7 @@ function populateStorage() {
   localStorage.setItem("fontfamily", document.getElementById("font").value);
   localStorage.setItem("image", document.getElementById("image").value);
   localStorage.setItem("fontcolor", document.querySelector("#fontcolor").value);
+  localStorage.setItem("note", document.querySelector("#textArea").value);
   setStyles();
 }
 
@@ -35,6 +50,7 @@ function setStyles() {
   document.getElementById("bgcolor").value = currentBgColor;
   document.getElementById("font").value = currentFont;
   document.getElementById("image").value = currentImage;
+  document.getElementById("fontcolor").value = currentFontColor;
   document.querySelector(".font-color").value = currentFontColor;
 
   htmlElem.style.backgroundColor = currentBgColor;
@@ -51,3 +67,5 @@ fontColorInput.addEventListener("change", populateStorage);
 fontStyleSelect.addEventListener("change", populateStorage);
 // listen for change in image selection
 imageSrcSelect.addEventListener("change", populateStorage);
+// listen for changes in the textarea element
+textAreaInput.addEventListener("change", populateStorage);
